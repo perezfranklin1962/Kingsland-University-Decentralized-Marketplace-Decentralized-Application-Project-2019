c55cf820127d0a7e59063f94270519e43bdda0c7
perezfranklin1962 <57190643+perezfranklin1962@users.noreply.github.com>
2019-11-21 21:20:39 -0500

    Added Test Cases for the itemForSaleFromSellerExists method

--- test/FranklinDecentralizedMarketplaceTest.js
+++ test/FranklinDecentralizedMarketplaceTest.js
@@ -1393,6 +1393,14 @@ contract("FranklinDecentralizedMarketplace and FranklinDecentralizedMarketplaceM
 
 	it("FranklinDecentralizedMarketplace : test getNumberOfDifferentItemsBeingSoldBySeller : seller exists - just added", async () => {
 		let sellerAddress = accounts[randomAddressIndex];
+
+		assert.equal(await franklinDecentralizedMarketplaceContract.sellerExists(sellerAddress), false, "Seller does not exist!");
+		assert.equal(await franklinDecentralizedMarketplaceContract.getNumberOfDifferentItemsBeingSoldBySeller(sellerAddress), 0,
+			"Seller that does not exist should have zero different items to sell!");
+	});
+
+	it("FranklinDecentralizedMarketplace : test getNumberOfDifferentItemsBeingSoldBySeller : seller exists - just added", async () => {
+		let sellerAddress = accounts[randomAddressIndex];
 		let itemIpfsHash = "DummyItem";
 
 		await franklinDecentralizedMarketplaceContract.addItemForSale(itemIpfsHash, { from: sellerAddress });
@@ -1486,6 +1494,7 @@ contract("FranklinDecentralizedMarketplace and FranklinDecentralizedMarketplaceM
 			}
 		}
 	});
+	*/
 
 	// Below is what's returned in function calls that change the contract.
     /*
@@ -1569,7 +1578,38 @@ contract("FranklinDecentralizedMarketplace and FranklinDecentralizedMarketplaceM
 		assert.equal(await franklinDecentralizedMarketplaceContract.getQuantityAvailableForSaleOfAnItemBySeller(accounts[randomAddressIndex], "DummyItem_2"), 4,
 			"Quantity available for Sale of the Item that was set is NOT equal to what is stored in the Contract!");
 	});
-	*/
+
+	it("FranklinDecentralizedMarketplace : test itemForSaleFromSellerExists : seller does not exist", async () => {
+		let sellerAddress = accounts[randomAddressIndex];
+		let itemIpfsHash = "DummyItem";
+
+		assert.equal(await franklinDecentralizedMarketplaceContract.sellerExists(sellerAddress), false, "Seller should not exist!");
+		assert.equal(await franklinDecentralizedMarketplaceContract.itemForSaleFromSellerExists(sellerAddress, itemIpfsHash), false,
+			"Seller that does not exist should also have no items ro sell that exist!");
+	});
+
+	it("FranklinDecentralizedMarketplace : test itemForSaleFromSellerExists : seller exists but item type in question does not exist for the seller", async () => {
+		let sellerAddress = accounts[randomAddressIndex];
+		let itemIpfsHash = "DummyItem";
+
+		await franklinDecentralizedMarketplaceContract.addItemForSale("DummyItem_Exists", { from: sellerAddress });
+		assert.equal(await franklinDecentralizedMarketplaceContract.sellerExists(sellerAddress), true,
+			"Seller that has at leat one item type to sell should always exist!");
+		assert.equal(await franklinDecentralizedMarketplaceContract.itemForSaleFromSellerExists(sellerAddress, itemIpfsHash), false,
+			`Seller does not have the ${itemIpfsHash} Item Type in question!`);
+	});
+
+	it("FranklinDecentralizedMarketplace : test itemForSaleFromSellerExists : seller exists and item type in question does exist for the seller", async () => {
+		let sellerAddress = accounts[randomAddressIndex];
+		let itemIpfsHash = "DummyItem";
+
+		await franklinDecentralizedMarketplaceContract.addItemForSale("DummyItem_Exists", { from: sellerAddress });
+		await franklinDecentralizedMarketplaceContract.addItemForSale(itemIpfsHash, { from: sellerAddress });
+		assert.equal(await franklinDecentralizedMarketplaceContract.sellerExists(sellerAddress), true,
+			"Seller that has at leat one item type to sell should always exist!");
+		assert.equal(await franklinDecentralizedMarketplaceContract.itemForSaleFromSellerExists(sellerAddress, itemIpfsHash), true,
+			`Seller does have the ${itemIpfsHash} Item Type in question!`);
+	});
 
 	// Below Unit Test done due to being suggested by Patrick Galloway. It took about 896392 milli-seconds to complete.
 	// It should be commented out, since it's highly repetitive and takes a long time to run. If run, it should be the last one to run.
